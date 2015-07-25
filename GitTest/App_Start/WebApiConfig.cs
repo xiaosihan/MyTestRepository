0efcac74cf8ddbd85b119a77c5e28d92200153a3
xiaosihan <423343700@qq.com>
2015-07-26 03:10:25 +0800

    delete and update

--- GitTest/App_Start/WebApiConfig.cs
+++ GitTest/App_Start/WebApiConfig.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json.Serialization;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Web.Http;
-using Microsoft.Owin.Security.OAuth;
-using Newtonsoft.Json.Serialization;
 
 namespace GitTest
 {
@@ -12,13 +11,10 @@ namespace GitTest
     {
         public static void Register(HttpConfiguration config)
         {
-            // Web API 配置和服务
-            // 将 Web API 配置为仅使用不记名令牌身份验证。
-            config.SuppressDefaultHostAuthentication();
-            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
-
             // Web API 路由
             config.MapHttpAttributeRoutes();
+
+            
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
